8ee56c847884e8a40f307cb8452f52014d4fa733
Wagner Araujo <wagnercorp17@gmail.com>
2025-02-25 23:34:16 -0300

    Pages settings, todo, analytics have been created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-export default function Home() {
-  return(
+export default function Home(){
+  return (
     <div>
-    <h1>Página Inicial</h1>
-  </div>
+      <h1>Home 2</h1>
+    </div>
   )
 }
+
