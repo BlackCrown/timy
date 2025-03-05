ff614d2a69eb4836edde7e39710ed2dd049ee69c
Wagner Araujo <wagnercorp17@gmail.com>
2025-03-05 11:07:52 -0300

    Initiating the creation of the main page for the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 export default function Home(){
   return (
-    <div>
-      <h1>Home 2</h1>
-    </div>
+    <main className="bg-[red] h-[calc(100vh-(80px))] rounded-3xl px-8 py-6">
+      <div className="h-full">
+        <header className="flex place-content-between">
+          <div>
+        <h1>Today</h1>
+        <p>Segunda, 05/2024, 11:00</p>
+          </div>
+          <div>
+            <p>Atividades</p>
+          </div>
+        </header>
+      </div>
+    </main>
   )
 }
 
