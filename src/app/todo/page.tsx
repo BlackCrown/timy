1c16299460a55e70099fb69f1b598d62803a4ffa
Wagner Araujo <wagnercorp17@gmail.com>
2025-03-07 20:59:33 -0300

    Creating todo

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import {useState} from React;
 
 export default function todo() {
   return (
     <section className='bg-principal-light h-[calc(100vh-(80px))] rounded-t-3xl px-8 py-6'>
       <h1>Todo List</h1>
       <div>
-        <label className='block' htmlFor="todo">Adcionar atividade:</label>
+        <label className='block' htmlFor="task">Adcionar atividade:</label>
         <div className='relative'>
-        <input type="text" id='todo' className='w-[80%] h-[30px] rounded-l-xl align-center pl-2 mt-2 border-y-2 border-principal-dark'/>
+        <input type="text" id='task' className='w-[80%] h-[30px] rounded-l-xl align-center pl-2 mt-2 border-y-2 border-principal-dark'/>
         <button className='bg-white rounded-r-xl border-y-2 border-l-2 border-principal-dark w-[30px] h-[30px] mt-2 absolute top-0 self-middle'>+</button>
         </div>
       </div>
