e3863ecf754bfa8305b79cc58b45cd6df3bc3f50
Wagner Araújo <wagnercorp17@gmail.com>
2025-03-21 17:56:31 -0300

    Initianting the creation off TiMy site

---
+++ src/components/Header/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <Image src="" className='w-30 h-18 bg-blue-600' alt='Site logo, TiMy!'/>
+      </div>
+    </header>
+  )
+}
+
