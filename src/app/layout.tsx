86f89aff6d97aa98411ab7a8cd1b8afd7ee5d507
Wagner Araújo <wagnercorp17@gmail.com>
2025-03-21 19:09:21 -0300

    Header component creation for the pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <Header />
       <body>
+      <Header />
         {children}
       </body>
     </html>
--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -1,12 +1,39 @@
-import Image from 'next/image'
-import React from 'react'
+"use client"
+import React, { MouseEventHandler } from 'react'
+import { Menu } from "@deemlol/next-icons";
+import { Bell } from "@deemlol/next-icons";
 
 export default function Header() {
+  function handleClick(event: MouseEventHandler<PointerEvent>):void {
+    if (event instanceof MouseEvent) { 
+      console.log(event);
+    }
+  }
+
   return (
-    <header>
-      <div>
-        <Image src="" className='w-30 h-18 bg-blue-600' alt='Site logo, TiMy!'/>
+    <header className='flex place-content-between items-center rounded-b-3xl p-3 gap-4 bg-[#f2f0f0]'>
+      <div className='flex gap-4'>
+        <div>
+          <button onClick={handleClick} className='cursor-pointer'>
+          <Menu size={28} color="#000000" />
+          </button>
+        </div>
+      <h1>Dashborad</h1>
       </div>
+
+      <div>
+        <div className='w-30 h-15 rounded-2xl bg-blue-600'/>
+      </div>
+
+      <div className='flex gap-4'>
+        <Bell size={28} color='#000000' />
+        <p>Usuário Marcos</p>
+        <div>
+          <div className='w-8 h-8 rounded-full bg-red-600'/>
+        </div>
+      </div>
+
+
     </header>
   )
 }
