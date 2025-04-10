b505ea4114500b1d0ed444c937245eac4807a552
Wagner Araújo <wagnercorp17@gmail.com>
2025-04-10 20:56:43 -0300

    Added functional functions to Add, delete, complete and edit tasks, all working normally

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -4,34 +4,48 @@ import React from 'react';
 import { Edit3, Trash2 } from '@deemlol/next-icons';
 // import TodoInput from '@/utils/input';
 
+interface iTask{
+  text: string;
+  id: number;
+  completed: boolean;
+}
+
+const teste:iTask[] =[ 
+  {text: 'teste', id: 1, completed: false},
+  {text: 'teste2', id: 2, completed: false},
+  {text: 'teste3', id: 3, completed: false}
+]
+
 export default function Todo() {
-  const [todos, setTodos] = React.useState<string[]>([]);
+  const [todos, setTodos] = React.useState<iTask[]>(teste);
   const [value, setValue] = React.useState<string>('');
+
 
   function addTask(event: React.FormEvent) {
     event.preventDefault();
     if (value.trim() === '') return;
-    setTodos((prev) => [...prev, value]);
+    const newTask: iTask = { text: value, id: todos.length + 1, completed: false };
+    setTodos((prev: iTask[]) => [...prev, newTask]);
     setValue('');
   }
 
-  function deletTask(event: React.MouseEvent) {
-    event.preventDefault();
-    const id = +event.currentTarget.id;
-
-    setTodos((prevList) => prevList.filter((_, index) => index !== id));
+  function deletTask(id: number) {
+    console.log(id);
+    console.log(todos);
+    setTodos(todos.filter((element) => element.id !== id));
   }
 
-  function editTask(event: React.MouseEvent) {
-    event.preventDefault();
-    const id = +event.currentTarget.id;
-    const newTask = prompt('Enter the new task', todos[id]);
-    if (newTask) {
-      setTodos((prevList) => {
-        const updatedList = [...prevList];
-        updatedList[id] = newTask;
-        return updatedList;
-      });
+  function toggleCompleteTask(id: number) {
+    const taskComplete = todos.filter(task => task.id === id);
+    if(taskComplete){
+      setTodos(todos.map(task => task.id === id ? {...task, completed: !task.completed}: task))
+    }
+  }
+
+  function editTask(id: number) {
+    const newTaskText = prompt('Enter the new task', todos.filter((task) => task.id === id)[0].text);
+    if(newTaskText !== null && newTaskText !== '') {
+      setTodos(todos.map(task => task.id === id ? {...task, text: newTaskText}: task))
     }
   }
 
@@ -64,23 +78,24 @@ export default function Todo() {
       <div className='bg-gray-200 place-items-center  rounded-md py-4 px-4 mt-4'>
         <h1 className='text-2xl font-bold p-2'>Your Tasks</h1>
         <ul className="w-full">
-          {todos.map((task, index) => (
+          {todos.map((task) => (
             <div
-              className="my-2 bg-gray-400 p-1 rounded-md"
-              key={index}>
+              className={task.completed ? "my-2 bg-red-400 p-1 rounded-md" : "my-2 bg-gray-400 p-1 rounded-md"}
+              key={task.id}>
               <li className="flex mx-auto max-w-xl justify-between break-all">
-                {task}
+              <div onClick={() => toggleCompleteTask(task.id)} className="flex w-4 h-4 rounded-1 border-2 border-blue-500"/>
+                {task.text}
                 <div className="flex ml-6 gap-5">
-                  <button
-                    onClick={deletTask}
+                  <div
+                    onClick={() => deletTask(task.id)}
                     className="text-white cursor-pointer"
-                    id={index.toString()}>
+                    id={`${task.id}`}>
                     <Trash2 className="text-red-100 fill-red-400" />
-                  </button>
+                  </div>
                   <button
-                    onClick={editTask}
+                    onClick={() => editTask(task.id)}
                     className=" text-white cursor-pointer px-1.5"
-                    id={index.toString()}>
+                    id={`${task.id}`}>
                     <Edit3
                       size={25}
                       className="text-green-900 fill-green-300"
